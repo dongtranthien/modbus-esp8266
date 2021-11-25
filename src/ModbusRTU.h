808a8089763e08b8846a36321adee9a8933d9b7f
Đông Trần Thiên <thiendong.iuh@gmail.com>
2021-11-25 14:13:30 +0700

    Update ModbusRTU.h

--- src/ModbusRTU.h
+++ src/ModbusRTU.h
@@ -23,6 +23,11 @@ class ModbusRTUTemplate : public Modbus {
 		uint8_t* _sentFrame = nullptr;
 		TAddress _sentReg = COIL(0);
 		uint16_t maxRegs = 0x007D;
+	
+		uint16_t counterPoll = 0;
+		bool isStartCheckFrequency = true;
+		uint32_t timeStartCheckFrequency;
+		float frequencyPoll = 0;
 
 		uint16_t send(uint8_t slaveId, TAddress startreg, cbTransaction cb, uint8_t unit = MODBUSIP_UNIT, uint8_t* data = nullptr, bool waitResponse = true);
 		// Prepare and send ModbusRTU frame. _frame buffer and _len should be filled with Modbus data
@@ -46,6 +51,7 @@ class ModbusRTUTemplate : public Modbus {
 		uint8_t server() { return _slaveId; }
 		inline uint8_t slave() { return server(); }
 		uint32_t eventSource() override {return _slaveId;}
+		float getFrequencyWorking();
 };
 
 template <class T>
